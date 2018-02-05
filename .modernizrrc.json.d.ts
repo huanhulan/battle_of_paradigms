4b02f30d611c45b5b83cc8e2d611432b3ab057c0
huanhulan <531971909@qq.com>
2018-02-05 23:11:59 +0800

    feat(statics/): add operational pictures

---
+++ .modernizrrc.json.d.ts
@@ -0,0 +1,7 @@
+declare namespace modernizrConfig {
+    const cssvhunit: boolean;
+    const cssgrid: boolean;
+    const flexbox: boolean;
+    const canvas: boolean;
+}
+export = modernizrConfig;
--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,30 @@ import {INIT_DOC} from './consts';
 import './style/index.scss';
 import {minimal_frp, dragging_frp, fixed_frp, shift_frp, refined_shift_frp} from './frp';
 import {Minimal, Dragging, Fixed, Shift} from './operational';
+import * as modernizrConfig from "./../.modernizrrc.json";
 
+const modernizr = modernizrConfig;
+const $app = document.getElementById("viewport");
 const initDoc = Array.prototype.slice.call(INIT_DOC);
 
-// minimal frp;
-makeFrpCanvas('minimal_frp', initDoc, minimal_frp);
-makeFrpCanvas('dragging_frp', initDoc, dragging_frp);
-makeFrpCanvas('fixed_frp', initDoc, fixed_frp);
-makeFrpCanvas('shift_frp', initDoc, shift_frp);
-makeFrpCanvas('refine_shift_frp', initDoc, refined_shift_frp);
+window.onload = () => {
+    if (!$app) {
+        return;
+    }
+    if (!modernizr.cssvhunit || !modernizr.cssgrid || !modernizr.flexbox || !modernizr.canvas) {
+        $app.innerHTML = 'please use modern browsers like Chrome to get the best user experience.'
+        return;
+    }
+    // minimal frp;
+    makeFrpCanvas('minimal_frp', initDoc, minimal_frp);
+    makeFrpCanvas('dragging_frp', initDoc, dragging_frp);
+    makeFrpCanvas('fixed_frp', initDoc, fixed_frp);
+    makeFrpCanvas('shift_frp', initDoc, shift_frp);
+    makeFrpCanvas('refine_shift_frp', initDoc, refined_shift_frp);
 
 // operational
-new Minimal('minimal_op', initDoc);
-new Dragging('dragging_op', initDoc);
-new Fixed('fixed_op', initDoc);
-new Shift('shift_op', initDoc);
+    new Minimal('minimal_op', initDoc);
+    new Dragging('dragging_op', initDoc);
+    new Fixed('fixed_op', initDoc);
+    new Shift('shift_op', initDoc);
+};
